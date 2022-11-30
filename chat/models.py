708c3a9444c12834fbf3e144c08283e69ad65402
Alisher Ubuntu <alijamil5420@gmail.com>
2022-11-30 16:47:14 +0500

    models created

--- chat/models.py
+++ chat/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     text = models.TextField('Text')
     time_created = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, models.CASCADE)
+    sent_id = models.ForeignKey(User, models.CASCADE, related_name='sent_to')
+    owner_id = models.ForeignKey(User, models.CASCADE, related_name='owner')
 
     def __str__(self):
         return self.text
+
+class Contact(models.Model):
+    contact_id = models.ManyToManyField(User)
+    contact_owner_id = models.ForeignKey(User, models.CASCADE, related_name='contact_owner')
+
