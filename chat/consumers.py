7dbe25c9576bd499581c07d9ecb0d220a9f2d94f
Alisher Ubuntu <alijamil5420@gmail.com>
2022-12-09 13:22:30 +0500

    sender can view message instantly

--- chat/consumers.py
+++ chat/consumers.py
@@ -26,15 +26,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-
+        user = text_data_json["user_id"]
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name, {"type": "chat_message",
+                                   "user": user,
+                                   "message": message}
         )
 
     # Receive message from room group
     async def chat_message(self, event):
+        user_id = event['user']
         message = event["message"]
         #
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({
+            'user_id': user_id,
+            "message": message
+        }))
