82ef79317f2c5fbb2d81d4649c0839b810fbb8fd
Alisher Ubuntu <alijamil5420@gmail.com>
2022-12-30 11:25:36 +0500

    added unique_room model into admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Contact, Message
+from .models import Contact, Message, Unique_room
 
 admin.site.register(Contact)
 admin.site.register(Message)
+admin.site.register(Unique_room)
