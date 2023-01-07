1ad61a481b08ecb585ed1cbbaaf17d17bac9da22
Alisher Ubuntu <alijamil5420@gmail.com>
2023-01-07 23:09:31 +0500

    5th attempt to deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nnfjmj&6u83nx9-o$3xf6i_qs!%boc6)x9j7u^m1pulr85hc*%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-194a.up.railway.app', '127.0.0.1:8000']
 
