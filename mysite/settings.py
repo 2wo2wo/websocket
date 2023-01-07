f513cf155f189f1c7d83c770d6a2b17b73a5003d
Alisher Ubuntu <alijamil5420@gmail.com>
2023-01-07 17:33:44 +0500

    created apps file which has bben deleted while ago

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat',
+    'chat.apps.ChatConfig',
     # 'rest_framework'
 ]
 
