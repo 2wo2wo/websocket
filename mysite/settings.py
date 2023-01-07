7c228123a0b26b33794d36be892e25a375116969
Alisher Ubuntu <alijamil5420@gmail.com>
2023-01-07 23:35:00 +0500

    8th attempt to deploy(redis)

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("containers-us-west-126.railway.app", 5479)],
         },
     },
 }
