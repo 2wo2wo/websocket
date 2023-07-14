df5383bf122871ccc365018ad4e3b87a91e2bf9c
Alisher Windows <alisherjonjamilov@gmail.com>
2023-07-14 19:18:17 +0500

    Added JWT authentication

--- chat/views_api.py
+++ chat/views_api.py
@@ -2,13 +2,22 @@ from .models import Contact
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ContactSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class ContactApi(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
     def get(self, request, format=None, *args, **kwargs):
         user = request.user
         contacts = Contact.objects.get(contact_owner_id=user)
         contact_serializer = ContactSerializer(contacts)
-        return Response(contact_serializer.data)
+        return Response({
+            'user': contact_serializer.data['contact_owner_id'],
+            'contacts': contact_serializer.data['contact_id']
+        })
+
+# class
 
