d7a5a1a2f4b43cbf4ed575a36dde702902b61484
Alisher Windows <alisherjonjamilov@gmail.com>
2023-07-16 15:49:47 +0500

    added User added API

--- chat/views_api.py
+++ chat/views_api.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from . import views
 from rest_framework.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class ContactApi(APIView):
     permission_classes = [IsAuthenticated]
@@ -32,3 +33,19 @@ class ContactSearchApi(APIView):
         return Response({
          "users": user_serializer.data
         })
+
+
+class AddUserContactApi(APIView):
+
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request, format=None, *args, **kwargs):
+        user_id = request.data['user_id']
+        try:
+            user_add = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response({'message': 'user not found'})
+        user_contacts, created = Contact.objects.get_or_create(contact_owner_id=request.user)
+        user_contacts.contact_id.add(user_add)
+        return Response({'message': 'user_added'})
