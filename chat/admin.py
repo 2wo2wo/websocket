4a0c0fd3d0496219d53f60753de8b31a3d756fd7
Alisher Windows <alisherjonjamilov@gmail.com>
2023-07-21 23:45:59 +0500

    code verification by email added

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Contact, Message, Unique_room
+from .models import Contact, Message, Unique_room, VerificationUser
 
 admin.site.register(Contact)
 admin.site.register(Message)
 admin.site.register(Unique_room)
+admin.site.register(VerificationUser)
