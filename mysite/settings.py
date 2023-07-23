0f9011a359b21a95ec0bc4494778b960755a9a4f
Alisher Windows <alisherjonjamilov@gmail.com>
2023-07-23 11:08:33 +0500

    Added cors headers configuration cors allow methods

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,10 +64,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,15 +75,30 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "POST",
+    "PUT",
+)
 
 CSRF_TRUSTED_ORIGINS = [
     os.environ.get("CSRF_URL"),
     "http://127.0.0.1:8000/",
     'https://www.2wo1ne.uz',
-    
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
 ]
 
 ROOT_URLCONF = 'mysite.urls'
