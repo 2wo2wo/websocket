16b34dc384ee013be6b23c57f528868678f823a5
Alisher Windows <alisherjonjamilov@gmail.com>
2023-08-19 18:20:15 +0500

    cors changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,7 +96,17 @@ CORS_ALLOW_METHODS = (
     "OPTIONS"
 )
 
-CORS_ALLOW_HEADERS = ('*',)
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
