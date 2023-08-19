2b8045794f1d71e34dc11af2448a0c5459ae1e0c
Alisher Windows <alisherjonjamilov@gmail.com>
2023-08-19 23:03:31 +0500

    access token and refresh token lifetime changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -247,8 +247,8 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
