ba4e02e2418365947aa31f84d3333b1adcce181c
Alisher Windows <alisherjonjamilov@gmail.com>
2023-09-15 11:06:01 +0500

    message admin changed

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Contact, Message, Unique_room, VerificationUser
+from django.utils.html import format_html
 
 admin.site.register(Contact)
-admin.site.register(Message)
 admin.site.register(Unique_room)
 admin.site.register(VerificationUser)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id_url', 'text', 'owner_id', 'sent_id', 'time_created')
+
+    def id_url(self, obj):
+        return format_html("<a href='{url}'>{url}</a>", url=obj.id)
--- user_handler_app/admin.py
+++ user_handler_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import User
 
+
 @admin.register(User)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("id_url", "email", "last_name", "first_name")
